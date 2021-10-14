ee6502a4b1c0f1c6da3fbedb77d66ad24f3dc540
huangshiping <huang951700@163.com>
2021-10-14 10:02:57 +0800

    huang

---
+++ src/components/Huang.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>黄世平</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Huang',
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ vue.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    //lintOnSave:false 关闭ESLint的检查规则
+    lintOnSave:'warning',//输出错误继续运行项目
+}
